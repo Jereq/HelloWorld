a7787aaca1a289f92596a6d888039ac8ea8f3de5
Sebastian Larsson <sjereq@gmail.com>
2011-04-09 21:51:21 +0200

    Edit message
    Add a space in "Hello World!"

--- src/Hello.java
+++ src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	 * @param args arguments
 	 */
 	public static void main(String[] args) {
-		System.out.println("HelloWorld!");
+		System.out.println("Hello World!");
 	}
 	
 }
