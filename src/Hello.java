7744796da4947510f35d142185b6835a24990973
Sebastian Larsson <sjereq@gmail.com>
2011-04-09 21:49:20 +0200

    Add Hello.java
    Simple test program added.

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	
+	/**
+	 * Implement the application
+	 * @param args arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+	}
+	
+}
